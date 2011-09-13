bbcad90cbbd3933f4c56f2dd67b15915b94feafa
Phuong Nguyen <phuong.nguyen@stanyangroup.com>
2011-09-13 11:54:33 +0700

    initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrangHo::Application.config.secret_token = '999f1bb69b231b2974abc53cb1119a0d4480cf5655a0141011d7d123d3723cd214312fe5f1d6de4a559a68757bad06a25962f7fe46005cf8b7cfb2594247a8fa'
